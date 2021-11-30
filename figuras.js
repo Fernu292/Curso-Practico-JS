13ed0cd19b4b1577f9fceb1d197dcbf071e65f82
Luis Fernando <fernu292@gmail.com>
2021-11-30 11:53:49 -0600

    Finalizando logica de Js

--- figuras.js
+++ figuras.js
@@ -5,32 +5,33 @@
 const Cuadrado = (e)=>{
     e.preventDefault();
     const input = document.getElementById('inputCuadrado');
-    const lado = input.value;
+    const lado = parseFloat(input.value);
 
     const perimetro_cuadrado = lado*4;
     const area_cuadrado = lado*lado;
 
-    alert(`El perimetro es: ${perimetro_cuadrado}m, y su area es: ${area_cuadrado}m2`);    
+    alert(`El perimetro es: ${perimetro_cuadrado}m, y su area es: ${area_cuadrado}m2`);
 }
 
 //Codigo del triangulo
 
 const Triangulo = (e)=>{
     e.preventDefault();
-    const lado1 = document.getElementById('input1').value;
-    const lado2 = document.getElementById('input2').value;
-    const lado3 = document.getElementById('input3').value;
+    let lado1 = parseFloat(document.getElementById('input1').value);
+    let lado2 = parseFloat(document.getElementById('input2').value);
+    let lado3 = parseFloat(document.getElementById('input3').value);
 
     let altura;
-    const perimetro_triangulo = lado1+lado2+lado3;
-    const area_triangulo = (lado1*altura) /2;
-
     if(lado1==lado2 && lado2==lado3){
         //Triangulo equilatero
         altura = Math.sqrt((lado1**2)-((lado2/2)**2));
         altura = altura.toPrecision(3);
-        alert(altura)
     }
+
+    const perimetro_triangulo = lado1+lado2+lado3;
+    const area_triangulo = (lado1*altura) /2;
+
+    alert(`El perimetro es: ${perimetro_triangulo}m y su area es: ${area_triangulo}m2`);
 
 }
 
@@ -38,21 +39,16 @@ const Triangulo = (e)=>{
 
 //Codigo del circulo
 
-const Circulo = (radio) =>{
+const Circulo = (e) =>{
+    e.preventDefault();
+    const radio = parseFloat(document.getElementById('radio').value);
+
     const diametro = radio*2;
     const Pi = Math.PI;
-    const perimetro = diametro * Pi;
-    const area = Pi * (radio*radio);
+    const perimetro = (diametro * Pi).toPrecision(7);
+    const area = (Pi * (radio*radio)).toPrecision(7);
 
-
-    console.group('Circulos');
-    
-    console.log(`El diametro del ciculo es: ${diametro}cm, y su radio es: ${radio}cm`);
-    console.log(`El perimetro del circulo es: ${perimetro}cm`);
-    console.log(`El area del circulo es: ${area}cm^2`);
-
-    console.groupEnd();
-
+    alert(`El area del ciculo es: ${area}m2 y su perimetro: ${perimetro}m`);
 }
 
 
